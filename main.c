afab401b5c1fb1675a7f38c190a4c4e9a97aa491
ptgmm <ptgmm@proton.me>
2024-10-23 00:45:56 +0200

    Fix small issue with .config checker; add more comments

--- main.c
+++ main.c
@@ -15,23 +15,20 @@ int main (int argc, char *argv[]) {
 
     const char *username = getenv("USER"); //Getting user name
     snprintf(fullpath, sizeof(fullpath), "/home/%s/.config/%s", username, argv[2]); //create full path to .config
-    
-    snprintf(folderCheck, sizeof(folderCheck), "/home/%s/.config/", username);
+    snprintf(folderCheck, sizeof(folderCheck), "/home/%s/.config/", username); //path to .config
 
     struct stat statbuf;
-    if (stat(folderCheck, &statbuf) != 0) {
-        config = mkdir(folderCheck, 0777);
-    }
-
-    if (argc > 1) {                                     //Basically checking if the user entered "init" and folder name 
-        if (strcmp(argv[1], "init") == 0 && argc == 3){ //
-            check = mkdir(fullpath, 0777);              // 
-            if (check == 0) {                           //
-            printf("Directory created\n");              //
-            } else {                                    //
-                perror("Unable to create directory");   //
-                exit(1);                                //
-            }
+    if (stat(folderCheck, &statbuf) != 0) {         //Check if .config exist
+        config = mkdir(folderCheck, 0777);          //If .config doesn't exist it will be created
+    } else if (stat(folderCheck, &statbuf) == 0 && argc > 1) {      //Basically checking if the user entered "init" and folder name 
+        if (strcmp(argv[1], "init") == 0 && argc == 3){             //
+            check = mkdir(fullpath, 0777);                          // 
+            if (check == 0) {                                       //
+            printf("Directory created\n");                          //
+            } else {                                                //
+                perror("Unable to create directory");               //
+                exit(1);                                            //
+            }                                                       //
 
         } else {
             printf("To create folder type 'init'\n");
