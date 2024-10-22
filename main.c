df06aa3441fb45127fda2410892c4015873e659c
ptgmm <ptgmm@proton.me>
2024-10-22 23:58:07 +0200

    Check if .config exist if not, program create .config

--- main.c
+++ main.c
@@ -9,11 +9,19 @@
 int main (int argc, char *argv[]) {
 
     int check;
+    int config;
     char fullpath[256];
+    char folderCheck[256];
 
     const char *username = getenv("USER"); //Getting user name
     snprintf(fullpath, sizeof(fullpath), "/home/%s/.config/%s", username, argv[2]); //create full path to .config
+    
+    snprintf(folderCheck, sizeof(folderCheck), "/home/%s/.config/", username);
 
+    struct stat statbuf;
+    if (stat(folderCheck, &statbuf) != 0) {
+        config = mkdir(folderCheck, 0777);
+    }
 
     if (argc > 1) {                                     //Basically checking if the user entered "init" and folder name 
         if (strcmp(argv[1], "init") == 0 && argc == 3){ //
