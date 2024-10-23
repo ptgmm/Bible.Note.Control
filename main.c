3b35d84d94db236f0179d5a3acce4ad45eaaf625
ptgmm <ptgmm@proton.me>
2024-10-24 00:43:42 +0200

    New Function: Add files to folder with notes

--- main.c
+++ main.c
@@ -3,40 +3,57 @@
 #include <sys/stat.h> 
 #include <dirent.h>
 #include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 
 
 int main (int argc, char *argv[]) {
 
     int check;
+    int addcheck;
     int config;
-    char fullpath[256];
+
+    char fullpathconfig[256];
+    char addpath[256];
     char folderCheck[256];
+    char add[256];
+    char cwd[PATH_MAX];
 
     const char *username = getenv("USER"); //Getting user name
-    snprintf(fullpath, sizeof(fullpath), "/home/%s/.config/%s", username, argv[2]); //create full path to .config
+    const char *pwd = getcwd(cwd, sizeof(cwd));
+
+    snprintf(fullpathconfig, sizeof(fullpathconfig), "/home/%s/.config/%s", username, argv[2]); //create full path to .config
+    snprintf(addpath, sizeof(addpath), "/home/%s/.config/%s/%s", username, argv[3], argv[2]); //create full path to .config
     snprintf(folderCheck, sizeof(folderCheck), "/home/%s/.config/", username); //path to .config
+    snprintf(add, sizeof(add), "%s/%s", pwd, argv[2]);
+    
 
     struct stat statbuf;
-    if (stat(folderCheck, &statbuf) != 0) {         //Check if .config exist
-        config = mkdir(folderCheck, 0777);          //If .config doesn't exist it will be created
+    if (stat(folderCheck, &statbuf) != 0) {                         //Check if .config exist
+        config = mkdir(folderCheck, 0777);                          //If .config doesn't exist it will be created
     } else if (stat(folderCheck, &statbuf) == 0 && argc > 1) {      //Basically checking if the user entered "init" and folder name 
         if (strcmp(argv[1], "init") == 0 && argc == 3){             //
-            check = mkdir(fullpath, 0777);                          // 
+            check = mkdir(fullpathconfig, 0777);                          // 
             if (check == 0) {                                       //
-            printf("Directory created\n");                          //
+                printf("Directory created\n");                      //
             } else {                                                //
                 perror("Unable to create directory");               //
                 exit(1);                                            //
             }                                                       //
-
-        } else {
-            printf("To create folder type 'init'\n");
+        } else if (strcmp(argv[1], "add") == 0 && argc == 4){
+            addcheck = rename(add, addpath);
+            if (addcheck == 0){
+                 printf("Good\n");
+            } else {
+                perror("Bad");
+             } 
         }
 
-    }  else {
-        printf("Too few arguments \n");
     }
+ 
+
+
 
     return 0;
 }
